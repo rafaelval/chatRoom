80576632e3e10a0492aa7139b3fa04cc616cb34d
rafael valbuena <artillero.satanas@gmail.com>
2025-02-06 12:32:54 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Ajusta según la estructura de tu proyecto
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

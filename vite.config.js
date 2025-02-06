74007cf6a67db458cd86dad30e1f8a83b76be610
rafael valbuena <artillero.satanas@gmail.com>
2025-02-06 12:52:31 -0500

    configuradas dependencias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/",
+  server: {
+    historyApiFallback: true,
+    port: 3000,
+  },
+  css: {
+    postcss: './postcss.config.js', // Asegúrate de que apunte al archivo PostCSS
+  },
+});
